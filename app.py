60c24eff2005141c768d230b14dcafbb45d34e43
M Tharun <tharun.m2022a@vitstudent.ac.in>
2024-04-17 21:03:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def get_dealer_orders(dealer_id):
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
